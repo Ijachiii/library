638ed68aa9998c668b6770400c4fc2dcdffebb40
David Audu <davidaudu1010@gmail.com>
2023-01-22 02:01:42 +0100

    url patterns

--- books/views.py
+++ books/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import *
+from .models import *
+
 
 # Create your views here.
+class BookListView(ListView):
+    model = Book
+    template_name = "book_list.html"
