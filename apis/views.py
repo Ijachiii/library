544ed088fdab58e78bbdddc0b99808a852e034d3
David Audu <davidaudu1010@gmail.com>
2023-01-22 15:18:41 +0100

    serializers

--- apis/views.py
+++ apis/views.py
@@ -1,5 +1,9 @@
 from rest_framework.generics import *
 from books.models import *
+
 from .serializers import BookSerializer
 
 # Create your views here.
+class BookAPIView(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
