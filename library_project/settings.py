6c3ae0940c64ef0449632a28705d0410f471e0f0
David Audu <davidaudu1010@gmail.com>
2023-01-22 14:21:10 +0100

    api

--- library_project/settings.py
+++ library_project/settings.py
@@ -32,12 +32,15 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "books.apps.BooksConfig",
+    "apis.apps.ApisConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # 3rd party
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
